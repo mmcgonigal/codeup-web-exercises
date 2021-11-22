3769741f25a2fa86085f4e0f5973b41049418a10
Mina McGonigal <mcgonigal.mina@gmail.com>
2021-11-22 12:38:46 -0600

    update

---
+++ primitives.js
@@ -0,0 +1,13 @@
+console.log(typeof 99.9);
+console.log(typeof "flase");
+console.log(typeof false);
+console.log(typeof '0');
+console.log(typeof 0);
+console.log(typeof true);
+console.log(typeof 'true');
+
+
+
+
+
+
