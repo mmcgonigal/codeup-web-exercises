8435353e6211cadb466ad148696fe126caf35609
Mina McGonigal <mcgonigal.mina@gmail.com>
2021-11-22 18:58:18 -0600

    update

--- operators.js
+++ operators.js
@@ -1,19 +1,19 @@
-console.log('1'+2);
-console.log(typeof '1' +2);
-console.log(6%4);
-console.log('3 + 4 is'+ 3 + 4);
-console.log(0<0);
-console.log('false' == false);
-console.log(5 >=-5);
-console.log(!false ||false);
-console.log(true|| "42");
-console.log(!true && !false);
-console.log(6%5)
-console.log(5<4 && 1 === 1);
-console.log(typeof 'codeup'==='string');
-console.log(4 >= 0 &&1 !== '1');
-console.log(6%3);
-console.log(5%2);
+console.log('1'+2); //"12"
+console.log(typeof '1' +2); //string number
+console.log(6%4);//2
+console.log('3 + 4 is'+ 3 + 4);//3+4 is7
+console.log(0<0);//false
+console.log('false' == false);//false
+console.log(5 >=-5);//true
+console.log(!false ||false);//true
+console.log(true|| "42");//true
+console.log(!true && !false); //false
+console.log(6%5) //1
+console.log(5<4 && 1 === 1); //false
+console.log(typeof 'codeup'==='string'); //true
+console.log(4 >= 0 &&1 !== '1'); //true
+console.log(6%3); //0
+console.log(5%2); //1
 
 
 
