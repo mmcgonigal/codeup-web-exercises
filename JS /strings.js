5ea51397b918f49361715bdda477c31f15193ee4
Mina McGonigal <mcgonigal.mina@gmail.com>
2021-11-24 08:16:46 -0600

    update

---
+++ JS /strings.js
@@ -0,0 +1,14 @@
+var myName = "Mina";
+var aSentence = "Hi!"+ " my"+ " name" + " is " + myName;
+console.log (aSentence);
+aSentence += " and I love cooking/\ baking.";
+console.log(aSentence);
+var final = "\nI just started learning coding and \n\tLearning coding is very difficult.";
+console.log (aSentence + final);
+
+
+
+
+
+
+
