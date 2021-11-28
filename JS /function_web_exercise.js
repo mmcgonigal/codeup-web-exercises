72c3f044e9199785c5ebe30c619dfe003d3faf24
Mina McGonigal <mcgonigal.mina@gmail.com>
2021-11-28 15:36:05 -0600

    update

---
+++ JS /function_web_exercise.js
@@ -0,0 +1,42 @@
+"use Strict";
+
+function isOdd(number){
+    if(number%2===1){
+        console.log("This is odd number")
+    }else{
+        console.log("This is even number")
+    }
+};
+isOdd(7);
+function isEven(number1){
+    if(number1%2===0){
+    console.log("This is even number")
+    }else{
+        console.log("This is odd number")
+    }
+}
+isEven(10);
+function identity(input){
+    console.log(input)
+}
+identity("banana");
+function addFive(input1){
+   console.log(input1+5)
+}
+addFive(10);
+addFive("banana");
+function isMultipleOfFive(number2){
+    console.log(number2*5)
+}
+isMultipleOfFive(10);
+function isThree(n){
+if(n===3){
+    console.log("true")
+}else{
+    console.log("false")
+}
+};
+isThree(3)
+function isMultipleOfThree(input2){
+
+}
