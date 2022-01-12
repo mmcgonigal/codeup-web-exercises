9741f9ce529cfce27f26526fb915ce4bb1ccea2d
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-01-12 11:51:44 -0600

    DOM class update

--- js/DOM-lecture.js
+++ js/DOM-lecture.js
@@ -41,11 +41,85 @@ console.log(p.textContent)
 
 // code block that will be executed when event happens. mostly this will be function. and declaring those functions is called event handler register.
 //event handler register : 이벤트가 발생하면, 이 함수를 호출해라 .
-const handleClick = function(){
+let handleClick = function(){
     alert("Welcome!")
 }
 
-const button = document.querySelector('button')
+let button = document.querySelector('button')
 button.onclick = handleClick
 //even if calling function, make sure not add () .
+
+
+
+
+
+//1. make a prompt to ask user for name
+//2, save the name to a variable.
+//3. input the name saved into the span.
+    //3-1 GET THE SPAN (or element) ,(id, class, tag name).
+    //3-2 save the retrieved span to a variable.
+    //3-3 change the inner contents of the span to the saved input variable
+//(prompAnswer)
+
+
+let promptAnswer = prompt('what is your name?');
+let retrievedSpan = document.getElementById("name");
+
+console.log('promptAnswer: ', promptAnswer);
+console.log('retrievedSpan: ', retrievedSpan);
+retrievedSpan.innerHTML = promptAnswer;
+
+
+
+//UPDATE ENTIRE HTML ELEMENT
+// document.getElementById('name').innerHTML="new TEXT"
+// retrievedSpan.innerHTML = promptAnswer;
+
+//JUST UPDATES TEXT
+// document.getElementById('name').textContent = "new text";
+// retrievedSpan.textContent = promptAnswer;
+
+let preText = document.getElementById('red');
+preText.textContent="It is a beautiful weather"
+
+//RETRIEVING MULTIPLE ELEMENTS:
+//GOOGLE : how to get elements by class using the DOM
+
+//1. Declare the variable.
+//2. document.getElementsByClassName
+//3. specify the class we're looking for -- > odd
+//4. console.log the variable to make sure I'm grabbing the info I need
+
+let myElements = document.getElementsByClassName('odd');
+console.log(myElements)
+// Returns ARRAY each element in the array is
+//each of the elements it found with the class name.
+//RETURNS ARRAY.
+
+// myElements.innerHTML = 'hello world'
+//this didn't work.
+
+//LOOP THROUGH AN ARRAY  to change the element returned
+
+for( let i = 0; i <= myElements.length; i++ ){
+    myElements[i].innerHTML="hello world";
+}
+
+
+
+
+
+//LET'S CHANGE THE COLOR OF MY H1 TAG TO PURPLE.
+    //1 ACCESS THE ELEMENT WE WANT TO CHANGE.
+    //2 CHANGE THE STYLE (attribute) to purple.
+    //3
+
+let myH1 = document.getElementById('greeting');
+
+//setAttribute('attribute name', 'attribute new value');
+myH1.setAttribute("style", "color: purple");
+
+
+
+
 
